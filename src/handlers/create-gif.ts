62dff154fcaf86b988ed748b96ba99f009508754
Julio Cesar Ceron <julio.ceorn@gmail.com>
2024-09-10 17:23:31 -0300

    Add handler createVideoComponent

--- src/handlers/create-gif.ts
+++ src/handlers/create-gif.ts
@@ -1,27 +1,5 @@
+import { createVideoComponent } from "./create-video-component"
+
 export const createGif = async (blob: Blob) => {
-  document.querySelector("video")?.remove()
-
-  const video = document.createElement("video")
-  video.setAttribute("controls", "true")
-  video.style.width = "900px"
-
-  document.body.appendChild(video)
-
-  video.src = URL.createObjectURL(blob)
-  await video.play()
-
-  video.onloadeddata = () => {
-    const canvas = document.createElement("canvas")
-    const ctx = canvas.getContext("2d")
-    canvas.width = video.videoWidth
-    canvas.height = video.videoHeight
-
-    video.addEventListener("timeupdate", () => {
-      ctx?.drawImage(video, 0, 0)
-    })
-
-    video.onended = () => {
-      console.log("Video ended")
-    }
-  }
+  await createVideoComponent(blob)
 }
