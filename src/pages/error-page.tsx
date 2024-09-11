a0737c1ab3c10750b365af57cc9f82fc81ded8df
Julio Cesar Ceron <julio.ceorn@gmail.com>
2024-09-10 21:28:19 -0300

    Fix lint wanings

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,14 +1,17 @@
 import { useRouteError } from "react-router-dom"
 
 export const ErrorPage = () => {
-  const error = useRouteError()
+  const { statusText = "", message = "" } = useRouteError() as {
+    statusText?: string
+    message?: string
+  }
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error?.statusText || error?.message}</i>
+        <i>{statusText || message}</i>
       </p>
     </div>
   )
