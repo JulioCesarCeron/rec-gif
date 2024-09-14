96ef6def9a0801d05267676322259a8453b2493a
Julio Cesar Ceron <julio.ceorn@gmail.com>
2024-09-14 10:08:03 -0300

    Add ffmpeg command to convert video to gif

--- src/handlers/create-gif.ts
+++ src/handlers/create-gif.ts
@@ -1,5 +1,27 @@
-import { createVideoComponent } from "./create-video-component"
+import { fetchFile } from "@ffmpeg/util"
+import { FFmpeg } from "@ffmpeg/ffmpeg"
 
-export const createGif = async (blob: Blob) => {
-  await createVideoComponent(blob)
+export const createGif = async (blob: Blob, ffmpeg: FFmpeg) => {
+  await ffmpeg.writeFile("video.mp4", await fetchFile(blob))
+
+  await ffmpeg.exec([
+    "-i",
+    "video.mp4",
+    "-vf",
+    "fps=30,scale=900:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse",
+    "-loop",
+    "0",
+    "image.gif",
+  ])
+
+  const fileData = await ffmpeg.readFile("image.gif")
+  const data = new Uint8Array(fileData as ArrayBuffer)
+  const imageGifBlob = new Blob([data], { type: "image/gif" })
+
+  document.querySelector("img")?.remove()
+
+  const image = document.createElement("img")
+
+  document.body.appendChild(image)
+  image.src = URL.createObjectURL(imageGifBlob)
 }
